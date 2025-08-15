ddf017ab1bf45bdfb3a55dab3b5658402b1950db
donywahyur <donyramadhan3011@gmail.com>
2025-08-15 07:30:32 +0700

    fix build client & express version

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,11 +5,12 @@ export default ({ req }) => {
 		return axios.create({
 			baseURL:
 				"http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
-			headers: req.headers,
+			headers: req?.headers,
 		});
 	} else {
 		return axios.create({
 			baseURL: "/",
+			withCredentials: true,
 		});
 	}
 };
